9a052079732edcf650ec670a426e0e1216b00ecb
mineBR55 <81719089+mineBR55@users.noreply.github.com>
2026-02-26 11:42:07 -0300

    Add files via upload

--- Action.lua
+++ Action.lua
@@ -1,20 +1,20 @@
 require("firecast.lua");
 local GUI = require("gui.lua");
 
-local function newfrmAction()
+local function constructNew_frmAction()
     local obj = GUI.fromHandle(_obj_newObject("form"));
     obj:setName("frmAction");
     return obj;
 end;
 
 local _frmAction = {
-    newEditor = newfrmAction,
-    new = newfrmAction,
+    newEditor = constructNew_frmAction,
+    new = constructNew_frmAction,
     name = "frmAction",
     dataType = "",
     formType = "undefined",
-    formComponentName = "form"};
+    formComponentName = "form"
+};
 
 Firecast.registrarForm(_frmAction);
-
 return _frmAction;
